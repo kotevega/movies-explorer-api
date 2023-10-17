fc79ec00c651066d2af90669277df3361782f831
Konstantin Logunov <keniglokon@yandex.ru>
2023-10-17 13:38:33 +0200

    cors

--- app.js
+++ app.js
@@ -14,20 +14,32 @@ const { PORT, DB_URL } = process.env;
 
 const app = express();
 
-mongoose.connect(process.env.NODE_ENV === 'production' ? DB_URL : 'mongodb://127.0.0.1:27017/bitfilmsdb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: false,
-});
+mongoose.connect(
+  process.env.NODE_ENV === 'production'
+    ? DB_URL
+    : 'mongodb://127.0.0.1:27017/bitfilmsdb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: false,
+  },
+);
 
 app.use(errorLogger);
 app.use(requestLogger);
 app.use(limiter);
-app.use(helmet());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://koloproject.nomoredomainsicu.ru'],
+    origin: [
+      'http://localhost:3000',
+      'https://koloproject.nomoredomainsicu.ru',
+    ],
     credentials: true,
   }),
 );
